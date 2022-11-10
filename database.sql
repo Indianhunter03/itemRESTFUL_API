13a9ba6a83486eed484158f1d5f9e0d10aa64075
Aditya Giri <101345477+Indianhunter03@users.noreply.github.com>
2022-11-10 18:09:11 +0530

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,11 @@
+CREATE TABLE items(
+  id integer,
+  name varchar,
+  sku integer,
+  brand varchar,
+  category varchar,
+  manufacturer varchar,
+  hsncode varchar,
+  weight float,
+  dimension varchar
+);
